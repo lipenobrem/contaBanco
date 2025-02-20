bf7925339880f6ba9481f7c43472b93b8ef16856
Felipe <Lipenobremartiniano2016@gmail.com>
2025-02-20 14:37:16 -0300

    atualizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,22 +2,30 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Por favor, digite o número da Conta:");
-        int numero = scanner.nextInt();
-        scanner.nextLine();
-        
-        System.out.println("Por favor, digite o número da Agência:");
-        String agencia = scanner.nextLine();
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
 
-        System.out.println("Por favor, digite o nome do Cliente:");
-        String nomeCliente = scanner.nextLine();
+        Scanner entrada =  new Scanner(System.in);
 
-        System.out.println("Por favor, digite o saldo:");
-        double saldo = scanner.nextDouble();
-        
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");    
-        scanner.close();
+        System.out.println("Por favor, digite o número da Conta!");
+        numeroConta = Integer.parseInt(entrada.nextLine());
+
+        System.out.println("Por favor, digite o número da Agência!");
+        agencia = entrada.nextLine();
+
+        System.out.println("Por favor, digite o Nome do Cliente!");
+
+        nomeCliente = entrada.nextLine();
+
+
+        System.out.println("Por favor, digite o Saldo da Conta!");
+        saldo = Double.parseDouble(entrada.nextLine());
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque");
+
+
+        entrada.close();
     }
 }
